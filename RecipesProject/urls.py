e9c60c222e0ac46473b9e695f91c0e52f9227b2b
Shruti Mall <shrutimall@Shrutis-MacBook-Air.local>
2024-02-04 01:28:13 +0530

    New File

--- RecipesProject/urls.py
+++ RecipesProject/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     
     path('',include('home.urls')),
     path('users/', include('users.urls')),
     path('recipes/', include('recipes.urls'))
